b5ac752eeb35167ad3e53ecc49b0863894a166d9
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-05 21:49:59 -0600

    created basic html and css files

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Isles Designs</title>
+	</head>
+	<body>
+		<header>
+
+		</header>
+		<p>hello world </p>
+		<footer>
+
+		</footer>
+	</body>
+</html>
