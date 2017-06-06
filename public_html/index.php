28d3d698e3742ff12efe9886bee83d5b983d74a0
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2017-06-05 22:00:31 -0600

    added header, bootstrap links, font awesome (etc.) and footer code from version 1

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="UTF-8">
 		<title>Isles Designs</title>
+
+		<!--For Internet explorer users please see http://lmgtfy.com/?q=how+to+download+google -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- my custom css-->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!--font awesome for icons and stuff-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- Google fonts for header and body-->
+		<link href="https://fonts.googleapis.com/css?family=Lovers+Quarrel|Offside" rel="stylesheet">
+
+		<!-- my custom css-->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.1/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.16.0/additional-methods.min.js"></script>
+
+		<!-- My JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+		
 	</head>
 	<body>
 		<header>
 
 		</header>
 		<p>hello world </p>
-		<footer>
 
+		<footer class="icon">
+			<div class="container text-center">
+				<div class="row">
+					<div>
+						<a href="https://www.linkedin.com/in/danielle-martin-6a2b21132"> <i
+								class="fa fa-linkedin-square fa-4x"></i></a>
+					</div> <!-- icon -->
+				</div> <!-- row -->
+				<div class="row">
+					<p>Connect with me on Linked-in and Thank you for stopping by</p>
+				</div>
+			</div> <!-- container -->
 		</footer>
 	</body>
 </html>
